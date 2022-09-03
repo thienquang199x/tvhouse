639bc7a92b2071dc998e33e56928b997d18f18e1
Quang Nguyễn Thiện <thienquang199x@gmail.com>
2022-09-03 18:10:14 +0700

    - update channels - update selected padding

--- app/src/main/java/com/ntq/tvhouse/MainActivity.kt
+++ app/src/main/java/com/ntq/tvhouse/MainActivity.kt
@@ -10,17 +10,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -131,8 +131,9 @@ fun HomeScreen(onNavigateToFriends: (channel:Channel) -> Unit) {
     } else {
         channels.addAll(adapter.fromJson(String(LocalContext.current.assets.open("channel_config.json").readBytes()))  ?: arrayListOf())
     }
-    Box() {
-        Image(painter = painterResource(id = R.drawable.img_bg), contentDescription = null, contentScale = ContentScale.FillWidth)
+
+    Box {
+        Image(painter = painterResource(id = R.drawable.img_bg), contentDescription = null, contentScale = ContentScale.FillBounds)
         Column(modifier = Modifier.padding(20.dp)) {
             Text(text = "Kênh truyền hình", color = Color.White, fontSize = 30.sp, fontWeight = FontWeight.Bold)
             Spacer(modifier = Modifier.height(10.dp))
@@ -140,10 +141,15 @@ fun HomeScreen(onNavigateToFriends: (channel:Channel) -> Unit) {
                 horizontalArrangement = Arrangement.spacedBy(5.dp)
             ){
                 items(channels){ channel ->
+                    var padding by remember { mutableStateOf(0.dp) }
                     Card(modifier = Modifier
                         .width(200.dp)
                         .aspectRatio(728f / 409)
                         .background(color = Color.Transparent)
+                        .padding(padding)
+                        .onFocusChanged {
+                            padding = if (it.isFocused) 0.dp else 10.dp
+                        }
                         .clickable {
                             onNavigateToFriends(channel)
                         },
